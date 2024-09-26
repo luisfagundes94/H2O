fdcb005b6b7f4b44dda7476f679409d7cecb3058
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-09-26 14:44:16 -0300

    Adding empty state to result

--- app/src/main/java/com/luisfagundes/h2o/core/common/result/Result.kt
+++ app/src/main/java/com/luisfagundes/h2o/core/common/result/Result.kt
@@ -9,6 +9,7 @@ sealed interface Result<out T> {
     data class Success<T>(val data: T) : Result<T>
     data class Error(val exception: Throwable) : Result<Nothing>
     data object Loading : Result<Nothing>
+    data object Empty : Result<Nothing>
 }
 
 fun <T> Flow<T>.asResult(): Flow<Result<T>> =
