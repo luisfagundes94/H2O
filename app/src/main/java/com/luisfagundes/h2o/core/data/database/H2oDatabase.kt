40e925f5f1fcb9311446f313fe65499e25ed0f83
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-09-26 11:26:50 -0300

    placing comma at the last param

--- app/src/main/java/com/luisfagundes/h2o/core/data/database/H2oDatabase.kt
+++ app/src/main/java/com/luisfagundes/h2o/core/data/database/H2oDatabase.kt
@@ -6,7 +6,11 @@ import com.luisfagundes.h2o.core.data.entity.WaterEntity
 
 const val DATABASE_NAME = "h2o_database"
 
-@Database(entities = [WaterEntity::class], version = 1)
+@Database(
+    entities = [WaterEntity::class],
+    version = 1,
+    exportSchema = false,
+)
 abstract class H2oDatabase : RoomDatabase() {
     abstract fun waterDao(): WaterDao
 }
