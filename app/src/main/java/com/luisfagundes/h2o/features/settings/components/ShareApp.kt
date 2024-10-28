e3d15e2c4a4e88814ac73db108a79adadfcc61a6
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-10-28 17:51:09 -0300

    Removing text button to avoid padding

--- app/src/main/java/com/luisfagundes/h2o/features/settings/components/ShareApp.kt
+++ app/src/main/java/com/luisfagundes/h2o/features/settings/components/ShareApp.kt
@@ -3,25 +3,26 @@ package com.luisfagundes.h2o.features.settings.components
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import com.luisfagundes.h2o.R
 
 @Composable
 fun ShareApp() {
     val context = LocalContext.current
-    TextButton(
-        onClick = { openBrowser(context) }
-    ) {
-        Text(
-            text = stringResource(R.string.share_app),
-            color = MaterialTheme.colorScheme.primary
-        )
-    }
+    Text(
+        modifier = Modifier.clickable { openBrowser(context) },
+        text = stringResource(R.string.share_app),
+        color = MaterialTheme.colorScheme.primary,
+        fontWeight = FontWeight.Bold,
+        fontSize = MaterialTheme.typography.titleMedium.fontSize
+    )
 }
 
 private fun openBrowser(context: Context) {
